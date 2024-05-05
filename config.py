53e159c7aed855dfe1f54fa594e4155540fce33d
Poison786 <168622740+Poison786@users.noreply.github.com>
2024-05-05 22:48:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 
-API_ID = API_ID = 22308019
+API_ID = API_ID = 24119778
 
-API_HASH = os.environ.get("API_HASH", "fe49bff9eaadccecb58ded07aff33e88")
+API_HASH = os.environ.get("API_HASH", "cca11ca97dd8683d65ca1beb62baceb1")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7152255228:AAGRJc1utg-XkhLfkhbGfGqLsxNUKbxvyw0")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7120409177:AAFCLgZvVnK0eEF5BzCRav7e7K6FVyjzcjQ")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-OWNER = int(os.environ.get("OWNER", 7079041090))
+OWNER = int(os.environ.get("OWNER", 7171191819))
 
-LOG = -1002036679367
+LOG = -1002074904583
 
